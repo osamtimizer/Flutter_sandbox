f6afc5498159dfb3d595d3ac9998ada5683bc343
osamtimizer <eaten.jb@gmail.com>
2020-01-13 22:32:05 +0900

    add one-line title column

--- lib/main.dart
+++ lib/main.dart
@@ -6,14 +6,55 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          // Expanded widget will stretch child widget.
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              // There are 2 children here.
+              //
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    'Oeschinen Lake Campground',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold
+                    ),
+                  ),
+                ),
+                Text(
+                  'Kandersteg, Switzerland',
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                )
+              ],
+            ),
+          ),
+          // these are children of Row Widget.
+          Icon(
+              Icons.star,
+              color: Colors.red[500]
+          ),
+          Text('41'),
+        ],
+      ),
+    );
+
     return MaterialApp(
       title: 'Flutter layout Demo',
       home: Scaffold(
         appBar: AppBar(
           title: Text('flutter layout demo'),
         ),
-        body: Center(
-          child: Text("Hello world"),
+        body: Column(
+          children: [
+            titleSection,
+          ],
         ),
       ),
     );
