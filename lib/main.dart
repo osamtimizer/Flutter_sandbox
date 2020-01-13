4378fed2638e0e139d046bc92e18b15ab30657ab
osamtimizer <eaten.jb@gmail.com>
2020-01-14 08:30:39 +0900

    add text section

--- lib/main.dart
+++ lib/main.dart
@@ -80,6 +80,20 @@ class MyApp extends StatelessWidget {
       ),
     );
 
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Text(
+        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+            'Alps. Situated 1,578 meters above sea level, it is one of the '
+            'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+            'half-hour walk through pastures and pine forest, leads you to the '
+            'lake, which warms to 20 degrees Celsius in the summer. Activities '
+            'enjoyed here include rowing, and riding the summer toboggan run.',
+        // "softwrap: true" means, Text lines will fill the column width before wrapping at a word boundary.
+        softWrap: true,
+      ),
+    );
+
     return MaterialApp(
       title: 'Flutter layout Demo',
       home: Scaffold(
@@ -90,6 +104,7 @@ class MyApp extends StatelessWidget {
           children: [
             titleSection,
             buttonSection,
+            textSection,
           ],
         ),
       ),
