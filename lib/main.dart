80020c119c686c091bd5d9fc3abd30aafe7b28a3
osamtimizer <eaten.jb@gmail.com>
2020-01-15 01:13:13 +0900

    Navigate to a new screen and back

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,59 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(HeroApp());
+// Terminology: In Flutter, screens and pages are called routes.
+// The remainder of this recipe refers to routes.
 
-class HeroApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'transition demo',
-      home: MainScreen(),
-    );
-  }
+// In Android, a route is equivalent to an Activity.
+// In iOS, a route is equivalent to a ViewController.
+// In Flutter, a route is just a widget.
+void main() {
+  runApp(MaterialApp(
+    title: 'Navigation Basics',
+    home: FirstRoute(),
+  ));
 }
 
-class MainScreen extends StatelessWidget {
+class FirstRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Main Screen'),
+        title: Text('First Route'),
       ),
-      body: GestureDetector(
-        onTap: () {
-          Navigator.push(context, MaterialPageRoute(builder: (_) {
-            return DetailScreen();
-          }));
-        },
-        child: Hero(
-          tag: 'imageHero',
-          child: Image.network(
-              'https://picsum.photos/250?image=9'
-          ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Open Route'),
+          onPressed: () {
+            // Navigate to Second Route when tapped.
+            // Navigator.push method adds a Route to the stack of routes managed by Navigator.
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SecondRoute()),
+            );
+          },
         ),
-      ),
+      )
     );
   }
 }
 
-class DetailScreen extends StatelessWidget {
+class SecondRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GestureDetector(
-        onTap: () {
-          Navigator.pop(context);
-        },
-        child: Hero(
-          tag: 'imageHero',
-          child: Image.network(
-              'https://picsum.photos/250?image=9'
-          ),
+        appBar: AppBar(
+          title: Text('Second Route'),
         ),
-      ),
+        body: Center(
+          child: RaisedButton(
+            onPressed: () {
+              // Navigate to First Route when tapped.
+              // Navigator.pop method removes current route from the stack of routes.
+              Navigator.pop(context);
+            },
+            child: Text('Go Back!'),
+          ),
+        )
     );
   }
 }
