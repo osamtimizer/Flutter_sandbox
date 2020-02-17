2382ed3c375c1f5486dab1c5712da1af3133a53d
osamtimizer <eaten.jb@gmail.com>
2020-02-18 01:59:10 +0900

    Return data from a screen

--- lib/main.dart
+++ lib/main.dart
@@ -6,146 +6,93 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Provide a function to handle named routes. Use this function to
-      // identify the named route being pushed, and create the correct
-      // Screen.
-        onGenerateRoute: (settings) {
-          // If you push the PassArguments route
-          if (settings.name == PassArgumentsScreen.routeName) {
-            // Cast the arguments to the correct type: ScreenArguments.
-            final ScreenArguments args = settings.arguments;
-
-            // Then, extract the required data from the arguments and
-            // pass the data to the correct screen.
-            return MaterialPageRoute(
-              builder: (context) {
-                return PassArgumentsScreen(
-                  title: args.title,
-                  message: args.message,
-                );
-              },
-            );
-          }
-        },
-        title: 'Navigation with Arguments',
-        home: HomeScreen(),
-        routes: {
-          ExtractArgumentsScreen.routeName: (context) =>
-              ExtractArgumentsScreen(),
-        });
+      title: "Returning Data",
+      home: HomeScreen(),
+    );
   }
 }
+
+// return data from a new screen.
+
+// 1. Define the house screen
+// 2. Add a button that launches the selection screen
+// 3. Show the selection screen with two buttons
+// 4. When a button is tapped, close the selection screen
+// 5. Show a snackBar on the home screen with the selection
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home Screen'),
+        title: Text("Returning Data Demo"),
+      ),
+      body: Center(child: SelectionButton()),
+    );
+  }
+}
+
+class SelectionButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      onPressed: () {
+        _navigationAndDisplaySelection(context);
+      },
+      child: Text('pick an option, any option!'),
+    );
+  }
+
+  _navigationAndDisplaySelection(BuildContext context) async {
+    final result = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => SelectionScreen()),
+    );
+
+    Scaffold.of(context)
+    ..removeCurrentSnackBar()
+    ..showSnackBar(SnackBar(content: Text("$result")));
+  }
+}
+
+class SelectionScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('pick an option'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            // A button that navigates to a named route that. The named route
-            // extracts the arguments by itself.
-            RaisedButton(
-              child: Text("Navigate to screen that extracts arguments"),
-              onPressed: () {
-                // When the user taps the button, navigate to a named route
-                // and provide the arguments as an optional parameter.
-                Navigator.pushNamed(
-                  context,
-                  ExtractArgumentsScreen.routeName,
-                  arguments: ScreenArguments(
-                    'Extract Arguments Screen',
-                    'This message is extracted in the build method.',
-                  ),
-                );
-              },
-            ),
-            // A button that navigates to a named route. For this route, extract
-            // the arguments in the onGenerateRoute function and pass them
-            // to the screen.
-            RaisedButton(
-              child: Text("Navigate to a named that accepts arguments"),
-              onPressed: () {
-                // When the user taps the button, navigate to a named route
-                // and provide the arguments as an optional parameter.
-                Navigator.pushNamed(
-                  context,
-                  PassArgumentsScreen.routeName,
-                  arguments: ScreenArguments(
-                    'Accept Arguments Screen',
-                    'This message is extracted in the onGenerateRoute function.',
-                  ),
-                );
-              },
-            ),
-          ],
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: RaisedButton(
+                  onPressed: () {
+                    // pop here with "Yep"...
+                    Navigator.pop(context, "Yep!");
+                  },
+                  child: Text("Yep!"),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: RaisedButton(
+                  onPressed: () {
+                    // pop here with "Nope"
+                    Navigator.pop(context, "Nope.");
+                  },
+                  child: Text('Nope.'),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
-// A Widget that extracts the necessary arguments from the ModalRoute.
-class ExtractArgumentsScreen extends StatelessWidget {
-  static const routeName = '/extractArguments';
 
-  @override
-  Widget build(BuildContext context) {
-    // Extract the arguments from the current ModalRoute settings and cast
-    // them as ScreenArguments.
-    final ScreenArguments args = ModalRoute.of(context).settings.arguments;
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(args.title),
-      ),
-      body: Center(
-        child: Text(args.message),
-      ),
-    );
-  }
-}
-
-// A Widget that accepts the necessary arguments via the constructor.
-class PassArgumentsScreen extends StatelessWidget {
-  static const routeName = '/passArguments';
-
-  final String title;
-  final String message;
-
-  // This Widget accepts the arguments as constructor parameters. It does not
-  // extract the arguments from the ModalRoute.
-  //
-  // The arguments are extracted by the onGenerateRoute function provided to the
-  // MaterialApp widget.
-  const PassArgumentsScreen({
-    Key key,
-    @required this.title,
-    @required this.message,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(message),
-      ),
-    );
-  }
-}
-
-// You can pass any object to the arguments parameter. In this example,
-// create a class that contains both a customizable title and message.
-class ScreenArguments {
-  final String title;
-  final String message;
-
-  ScreenArguments(this.title, this.message);
-}
